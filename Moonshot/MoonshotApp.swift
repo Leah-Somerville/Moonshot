65fa03e4d67f836916aa1eca7e2638f7834dbd0d
Leah Somerville <leah.somerville4@gmail.com>
2024-03-08 09:52:07 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Leah Somerville on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
